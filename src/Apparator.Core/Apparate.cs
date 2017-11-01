89758eb94b49f041cf183a07e8b2703dd3ae55cb
Ryan Nowak <nowakra@gmail.com>
2017-10-31 23:37:41 -0700

    Doing things. Not well

---
+++ src/Apparator.Core/Apparate.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Build.Framework;
+using MSBuildTask = Microsoft.Build.Utilities.Task;
+
+namespace Apparator.Core
+{
+    public partial class Apparate : MSBuildTask
+    {
+        public MessageImportance MessageImportance { get; set; } = MessageImportance.High;
+
+        public bool Debug { get; set; }
+
+        [Required]
+        public string DepsFile { get; set; }
+
+        [Required]
+        public string EntryAssembly { get; set; }
+
+        // Needed because the Razor compiler is not a runtime library.
+        public string EntryAssemblyPath { get; set; }
+
+        [Required]
+        public string EntryPointType { get; set; }
+
+        [Required]
+        public string EntryPointMethod { get; set; }
+
+        [Required]
+        public string[] Args { get; set; }
+    }
+}
---
+++ src/Apparator.Core/Apparate.desktop.cs
@@ -0,0 +1,17 @@
+﻿#if NET462
+
+using System;
+using Microsoft.Build.Framework;
+using MSBuildTask = Microsoft.Build.Utilities.Task;
+
+namespace Apparator.Core
+{
+    public partial class Apparate
+    {
+        public override bool Execute()
+        {
+            throw new NotImplementedException("TODO Net46 implementation with appdomains. LOL");
+        }
+    }
+}
+#endif
