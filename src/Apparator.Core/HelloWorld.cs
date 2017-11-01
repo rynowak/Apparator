18bb368a2cdc63f18215c7ca11b128587f3c6d7b
Ryan Nowak <nowakra@gmail.com>
2017-10-31 21:11:38 -0700

    hello world

---
+++ src/Apparator.Core/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Build.Framework;
+using MSBuildTask = Microsoft.Build.Utilities.Task;
+
+namespace Apparator.Core
+{
+    public class HelloWorld : MSBuildTask
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public override bool Execute()
+        {
+            Log.LogMessage("Hello {0}", Name);
+            return true;
+        }
+    }
+}
