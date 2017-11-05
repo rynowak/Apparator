48453ed0452aa524e162f7c83ccb3f4acf2179b5
Ryan Nowak <nowakra@gmail.com>
2017-11-05 15:23:13 -0800

    Add Razor tasks
    Starting to work on a Razor 'Rzc' task that does Razor compilation
    inside MSBuild.
    
    Right now this just gets passed a list of the assemblies and the ouput
    path and logs it :)

---
+++ src/Apparator.Razor.Tasks/Rzc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.Build.Framework;
+using MSBuildTask = Microsoft.Build.Utilities.Task;
+
+namespace Apparator.Razor.Tasks
+{
+    public class Rzc : MSBuildTask
+    {
+        [Required]
+        public ITaskItem[] References { get; set; }
+
+        [Required]
+        public string OutputAssembly { get; set; }
+
+        public override bool Execute()
+        {
+            foreach (var reference in References)
+            {
+                Log.LogMessage(MessageImportance.High, "reference assembly: {0}", reference.ItemSpec);
+            }
+
+            Log.LogMessage(MessageImportance.High, "output assembly: {0}", OutputAssembly);
+
+            return true;
+        }
+    }
+}
